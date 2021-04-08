3036e61892734c8dcf844694e7b41f99d1e6741f
shiva-12345 <munnoorushiva07@gmail.com>
2021-04-08 17:19:56 +0530

    check two lines equal or not

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace Line_comparision
+namespace LineComparison
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("line problems");
+            Console.WriteLine("Wellcome To Line Comparison Computation Program!");
+            // CartesianSystem.lengthOFLine();
+            TwoLinesEqualOrNot.twoLinesEqualOrNot();
+
         }
     }
 }
